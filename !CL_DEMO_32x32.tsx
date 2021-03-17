94a9da641b61fbad19984cd9337711a180699a27
zwaehhhn <80837124+zwaehhhn@users.noreply.github.com>
2021-03-17 15:40:56 +0000

    Deploying to gh-pages from master @ ad92e2220c6f5146dcd3e35f45a7eef6e899b627 🚀

---
+++ !CL_DEMO_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.02.15" name="!CL_DEMO_32x32" tilewidth="32" tileheight="32" tilecount="680" columns="40">
+ <image source="!CL_DEMO_32x32.png" trans="ffffff" width="1280" height="544"/>
+</tileset>
---
+++ overworld_tileset_grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.02.15" name="overworld_tileset_grass" tilewidth="16" tileheight="16" tilecount="252" columns="12">
+ <image source="overworld_tileset_grass.png" trans="ffffff" width="192" height="336"/>
+</tileset>
