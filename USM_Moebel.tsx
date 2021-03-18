5c95ea28960b3fe6cf0437e72f058cb773c12d34
zwaehhhn <80837124+zwaehhhn@users.noreply.github.com>
2021-03-18 16:21:48 +0100

    s

---
+++ USM_Moebel.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="USM_Moebel" tilewidth="32" tileheight="32" tilecount="40" columns="5">
+ <image source="USM_Moebel.png" width="160" height="256"/>
+</tileset>
