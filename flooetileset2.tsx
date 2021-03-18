3c141b7d02ec3962cf481cd1f62db7c6ee79a89e
zwaehhhn <80837124+zwaehhhn@users.noreply.github.com>
2021-03-18 11:41:25 +0100

    Add files via upload

---
+++ flooetileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="floortileset2" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="64" columns="8">
+ <image source="floortileset2.png" width="272" height="272"/>
+</tileset>
---
+++ floortileset3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.02.15" name="floortileset3" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="64" columns="8">
+ <image source="floortileset_angepasst.png" width="272" height="272"/>
+</tileset>
