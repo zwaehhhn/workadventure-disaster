17144e6fe17c4e995bd8bba940ffc077125c699b
zwaehhhn <80837124+zwaehhhn@users.noreply.github.com>
2021-03-23 14:39:46 +0100

    update

--- Harburg1.tsx
+++ Harburg1.tsx
@@ -1,2 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Harburg1" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.4" tiledversion="1.4.3" name="Harburg1" tilewidth="32" tileheight="32" tilecount="16157" columns="151">
+ <image source="Harburg1.png" width="4850" height="3428"/>
+</tileset>
